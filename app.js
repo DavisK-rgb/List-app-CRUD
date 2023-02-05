161f83d768ae1154759635b52c04929e455c7580
Davis <katendedavis93@gmail.com>
2023-02-05 16:30:52 +0300

    added add student functionality

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 //setting view engine to ejs
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
+
 const students = [{fname:'Katende',lname:'Davis'},{fname:'Mawanda',lname:'Timo'},{fname:'Baingana',lname:'Joshua'}];
 
 
@@ -12,6 +15,18 @@ app.get('/',(req,res)=>{
     res.render('top',{title:'List-app',students:students});
 
 
+
+});
+
+app.get('/new',(req,res)=>{
+    res.render('new');
+
+});
+
+app.post('/new',(req,res)=>{
+
+students.push({fname:req.body.fname,lname:req.body.lname});
+res.redirect('/');
 
 });
 
